cefdaad6f40453b857600d0b83d739586b342b56
AAdi404 <71706403+AAdi404@users.noreply.github.com>
2021-04-24 15:56:03 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "BOOLEAN";
+const accessCode2 = "STRING";
+const accessCode3 = "CONSTRAINT";
+
+function clues() {
+    
+    fill("white");
+    textSize(15);
+    text("EAOLNOB", 100,50);
+    fill("lightblue");
+    text("Hint: It’s a data type !!", 100,70);
+
+    fill("white");
+    textSize(15);
+    text("IRTNSG", 700,150);
+    fill("lightblue");
+    text("Hint: It’s sequence of characters !!", 700,170);
+
+    fill("white");
+    textSize(15);
+    text("", 400,360);
+    fill("lightblue");
+    text("Hint: It joins two sprites !!", 400,380);
+    
+
+}
